ec04a7e1037c6a0d25b7946be12ab3b205622f62
lap <luuchilap@gmail.com>
2024-12-01 15:33:51 +0700

    Check login and Get average stars

--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
 import express from 'express';
 const app = express();
-import{read_nguoi_dung, read_hang_hoa} from './src/routes.js'
+import { read_nguoi_dung, read_hang_hoa, get_average_rating, check_login } from './src/routes.js'
+
+// Middleware để xử lý JSON trong request body
+app.use(express.json());
 
 // APIs
 app.get('/nguoi-dung', read_nguoi_dung);
 app.get('/hang-hoa', read_hang_hoa);
+app.get('/average-rating/:maHangHoa', get_average_rating);
+app.post('/login', check_login);  // API mới cho đăng nhập
 
 var server = app.listen(5000, function () {
     console.log('Server is running..');
--- server/src/routes.js
+++ server/src/routes.js
@@ -18,4 +18,33 @@ function read_hang_hoa(request, response) {
     });
 };
 
-export {read_nguoi_dung, read_hang_hoa}
+function get_average_rating(request, response) {
+    const maHangHoa = request.params.maHangHoa;
+    const sqlQuery = `SELECT dbo.GetAverageRatingForProduct(${maHangHoa}) AS AverageRating`;
+
+    query(sqlQuery, (error, result) => {
+        if (error) {
+            response.send(error)
+        } else {
+            response.send(result);
+        }
+    });
+}
+
+function check_login(request, response) {
+    const { username, password } = request.body;
+    
+    if (username === process.env.SQL_USERNAME && password === process.env.SQL_PASSWORD) {
+        response.json({
+            success: true,
+            message: "Đăng nhập thành công"
+        });
+    } else {
+        response.json({
+            success: false,
+            message: "Tên đăng nhập hoặc mật khẩu không đúng"
+        });
+    }
+}
+
+export {read_nguoi_dung, read_hang_hoa, get_average_rating, check_login}
